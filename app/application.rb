b7056cd6c41cd0bc05d0ce3bf64b3fb32d0973f6
Justin Daniel <justinddaniel@gmail.com>
2017-10-17 01:35:52 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,12 +3,12 @@ class Application
   resp = Rack::Response.new
   req = Rack::Request.new(env)
   item_name_array = []
-  item_name_array = @@items.collect {|i| i.name}
+  item_name_array = Item.all.collect {|i| i.name}
 
     if req.path.match(/items/)
       search_term = req.params["item"]
       if item_name_array.include?(search_term)
-        search_item = @@items.select{ |i| i.name == "#{search_term}" }
+        search_item = Item.all.select{ |i| i.name == "#{search_term}" }
         item_price = search_item.price
         resp.write "#{item_price}"
       else
