2c1726382aec6a5471427c74d011568b0bd4ca68
Justin Daniel <justinddaniel@gmail.com>
2017-10-17 01:24:38 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,21 +3,21 @@ class Application
   resp = Rack::Response.new
   req = Rack::Request.new(env)
 
-  if req.path.match(/items/)
-    search_term = req.params["item"]
-    if @@items.include?(search_term)
-      search_item = @@items.select{ |i| i.name == "#{search_term}" }
-      item_price = search_item.price
-      resp.write "#{item_price}"
+    if req.path.match(/items/)
+      search_term = req.params["item"]
+      if @@items.include?(search_term)
+        search_item = @@items.select{ |i| i.name == "#{search_term}" }
+        item_price = search_item.price
+        resp.write "#{item_price}"
+      else 
+        resp.write "Item not found"
+        resp.status = 400
+      end
     else 
-      resp.write "Item not found"
-      resp.status = 400
+      resp.write "Route not found"
+      resp.status = 404
     end
-  else 
-    resp.write "Route not found"
-    resp.status = 404
   end
-end
 
   
 end
